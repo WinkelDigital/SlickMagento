5d86ca046833af47e5fbf7ae77353d7fd2892f83
Patrick Star <hprasetyou@gmail.com>
2021-05-18 16:34:51 +0700

    init

---
+++ view/frontend/web/js/carousel-nav.js
@@ -0,0 +1,22 @@
+define(['jquery','Winkel_Carousel/libs/slick/slick'], function($,slick){
+    'use strict';
+
+    return function(config, element){
+      if(config.nextButton){
+        $(element).find(config.nextButton).click(e=>{
+          console.log(config.nextButton);
+          e.preventDefault()
+          $(config.target).slick('slickNext')
+        })
+      }
+      if(config.prevButton){
+        $(element).find(config.prevButton).click(e=>{
+          e.preventDefault()
+          $(config.target).slick('slickPrev')
+        })
+      }
+
+
+    }
+  }
+)
---
+++ view/frontend/web/js/carousel.js
@@ -0,0 +1,8 @@
+define(['jquery','Winkel_Carousel/libs/slick/slick'], function($,slick){
+    'use strict';
+
+    return function(config, element){
+      $(element).slick(config)
+    }
+  }
+)
